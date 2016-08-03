843779927115ff9e2918cad0e46ba2793ca90818
jmcimula <jmkacim@gmail.com>
2016-08-03 11:44:51 +0200

    Type of information to search

---
+++ R/info_type.R
@@ -0,0 +1,11 @@
+#'The package for Okapi News Tracker
+#'
+#'@author Jean Marie Cimula
+#'@details
+#' Type of news to track for articles of Okapi
+#' @export
+
+get_info_type <- function (){
+     y <- c("actualite","culture","environnement","politique","societe","sport","economie")
+	   print(y)
+}
